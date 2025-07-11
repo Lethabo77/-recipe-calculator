fe3f5bcfa69b8b08aaf531b50e077e806e39d79e
Lethabo Baloyi <happinesslethabo13@gmail.com>
2025-07-11 16:42:28 +0200

    Create main.tsx
    Create main App component

---
+++ src/main.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(</React.St
+**Commit message:** `Add React entry point`
+
+---
+
+### **File 3: `src/App.tsx`**
+**Filename:** `src/App.tsx`  
+**Content:**
+```tsx
+import RecipeCalculator from "./RecipeCalculator";
+
+function App() {
+  return ();
+}
+
+export default App;
